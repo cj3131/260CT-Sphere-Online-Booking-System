9f7d9a501817caf0a3ed286e5d1c09f25c5ca5ae
Tadas Kivilius <taduksaz@live.com>
2017-02-27 12:54:53 +0000

    Homepage, basic design

--- src/javascripts/JavaScript.js
+++ src/javascripts/JavaScript.js
@@ -1,10 +1,1 @@
-﻿function show(elementId) {
-    if (document.getElementById(elementId).style.display == "none")
-    {
-        document.getElementById(elementId).style.display = "block";
-    }
-    else
-    {
-        document.getElementById(elementId).style.display = "none";
-    }
-}
+﻿
