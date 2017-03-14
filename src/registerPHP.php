c9e4e3d52887a9eb81a37a16865ba59f75bb3e36
Callum Huntington <callum2online@msn.com>
2017-03-14 15:56:49 +0000

    Added registration tables

--- src/registerPHP.php
+++ src/registerPHP.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "myDB";
+$dbname = "sphere1_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,16 +11,47 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT id, firstname, lastname, email FROM MyGuests";
+echo "test 1";
+/*
+$firstname = $_POST["customer_firstname"];
+$surname = $_POST["customer_surname"];
+$email = $_POST["email_address"];
+$phonenumber = $_POST["phone_number"];
+$dob = $_POST["dob"];
+$password = $_POST["password"];
+$customer_address_one = $_POST["customer_address1"];
+$customer_address_two = $_POST["customer_address2"];
+$city = $_POST["customer_city"];
+$county = $_POST["customer_county"];
+$postcode = $_POST["customer_postcode"];
+
+echo "test 2";
+
+$sql = "INSERT INTO members (first_name, last_name, email, phone_number, dob, address_one, address_two, city, country, postcode, password)
+VALUES ('{$firstname}','{$surname}','{$email}','{$phonenumber}','{$dob}','{$customer_address_one}','{$customer_address_two}','{$city}',
+'{$county}','{$postcode}','{$password}')"; 
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+    
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+*/
+
+
+$sql = "SELECT * FROM members";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. " email: " . $row["email"]. "<br>";
+        echo " - Name: " . $row["user_id"]. "<br>";
     }
 } else {
     echo "0 results";
 }
 $conn->close();
+
 ?>
