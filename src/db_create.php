c4c28a7cd2a4f0a8536b08540cb19734d74de5e6
Patrick Johnson <johns347@uni.coventry.ac.uk>
2017-03-14 13:57:25 +0000

    Fix errors in db_create file

--- src/db_create.php
+++ src/db_create.php
@@ -5,14 +5,14 @@ $password = "";
 $dbname = "sphere_db";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-/*
+
 $sql = "CREATE DATABASE sphere_db";
 
 if ($conn->query($sql) === TRUE) {
@@ -20,37 +20,14 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "Error creating database: " . $conn->error;
 }
-*/
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-$sql = "CREATE TABLE sessions (
-session_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-date VARCHAR(30) NOT NULL,
-time VARCHAR(30) NOT NULL,
-group_id INT(6) UNSIGNED,
-CONSTRAINT sess_group_id_fk FOREIGN KEY (group_id)
-REFERENCES groups(group_id)
-)";
-if ($conn->query($sql) === TRUE) {
-    echo "Table sessions created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-
-
-$sql = "CREATE TABLE groups (
-group_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-attendees INT(3) NOT NULL,
-user_id INT(6) UNSIGNED,
-CONSTRAINT group_user_id_fk FOREIGN KEY (user_id)
-REFERENCES members(user_id)
-)";
-if ($conn->query($sql) === TRUE) {
-    echo "Table groups created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
 $sql = "CREATE TABLE members (
 user_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
@@ -68,6 +45,34 @@ password VARCHAR(20) NOT NULL
 )";
 if ($conn->query($sql) === TRUE) {
     echo "Table members created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$sql = "CREATE TABLE groups (
+group_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+attendees INT(3) NOT NULL,
+user_id INT(6) UNSIGNED,
+CONSTRAINT group_user_id_fk FOREIGN KEY (user_id)
+REFERENCES members(user_id)
+)";
+if ($conn->query($sql) === TRUE) {
+    echo "Table groups created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+
+$sql = "CREATE TABLE sessions (
+session_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+date VARCHAR(30) NOT NULL,
+time VARCHAR(30) NOT NULL,
+group_id INT(6) UNSIGNED,
+CONSTRAINT sess_group_id_fk FOREIGN KEY (group_id)
+REFERENCES groups(group_id)
+)";
+if ($conn->query($sql) === TRUE) {
+    echo "Table sessions created successfully";
 } else {
     echo "Error creating table: " . $conn->error;
 }
