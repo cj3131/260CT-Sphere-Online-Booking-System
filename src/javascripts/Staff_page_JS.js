e4caab6e6ee6157b17f1f5ba89548206d4383501
drakoshi <kiviliut@uni.coventry.ac.uk>
2017-04-04 21:30:33 +0000

    Clean up

--- src/javascripts/Staff_page_JS.js
+++ src/javascripts/Staff_page_JS.js
@@ -17,7 +17,7 @@ function makeRequest(rType,rSelect)
     
     $.ajax({        
         type: rType,
-        url: 'Controller_staff.php',
+        url: 'Staff_Controller.php',
         data: {"selector": rSelect,"data": rData},
         dataType: 'json',      
         success: function(data)
